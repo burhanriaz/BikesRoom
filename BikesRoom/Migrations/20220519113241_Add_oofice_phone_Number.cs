7a418058f5f94d834260ddba6961d555b16de443
Burhan <Burhan@DESKTOP-9NRFKA1>
2022-05-19 16:33:05 +0500

    add-Migration Add ofice phone Number

---
+++ BikesRoom/Migrations/20220519113241_Add_oofice_phone_Number.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BikesRoom.Migrations
+{
+    public partial class Add_oofice_phone_Number : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
